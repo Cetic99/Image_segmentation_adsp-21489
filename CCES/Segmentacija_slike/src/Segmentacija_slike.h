aaaf0ae54731183b2073eee0b0945f789ed7431f
Nikola Cetic <nikolacetic8@gmail.com>
2023-02-04 23:13:32 +0100

    Issue #5 : Modify Natural C code to run on dsp Now it is able to run on DSP

---
+++ CCES/Segmentacija_slike/src/Segmentacija_slike.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Segmentacija_slike.h
+ *****************************************************************************/
+
+#ifndef __SEGMENTACIJA_SLIKE_H__
+#define __SEGMENTACIJA_SLIKE_H__
+
+/* Add your custom header content here */
+
+#endif /* __SEGMENTACIJA_SLIKE_H__ */
